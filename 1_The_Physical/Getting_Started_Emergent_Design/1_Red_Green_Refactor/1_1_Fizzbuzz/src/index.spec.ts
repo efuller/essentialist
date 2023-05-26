45c7175533a7fa0bd8433d2f02e93cb25081f88c
Eric Fuller <info@glassmoonmedia.com>
2023-05-26 15:40:52 -0400

    fix indentation

--- 1_The_Physical/Getting_Started_Emergent_Design/1_Red_Green_Refactor/1_1_Fizzbuzz/src/index.spec.ts
+++ 1_The_Physical/Getting_Started_Emergent_Design/1_Red_Green_Refactor/1_1_Fizzbuzz/src/index.spec.ts
@@ -1,41 +1,41 @@
-import { fizzBuzzChecker } from "./fizzbuzz";
+import {fizzBuzzChecker} from "./fizzbuzz";
 
 describe("fizzbuzz", () => {
-    const errorMessage = 'Numbers between 1 and 100 only.';
+  const errorMessage = 'Numbers between 1 and 100 only.';
 
-    it('should know that fizzBuzzChecker exists', () => {
-        expect(fizzBuzzChecker).toBeDefined();
-    });
+  it('should know that fizzBuzzChecker exists', () => {
+    expect(fizzBuzzChecker).toBeDefined();
+  });
 
-    it('should return a string', () => {
-        const result = fizzBuzzChecker(25);
+  it('should return a string', () => {
+    const result = fizzBuzzChecker(25);
 
-        expect(typeof result).toBe('string');
-    });
+    expect(typeof result).toBe('string');
+  });
 
-    it.each([
-        102,
-        -12,
-        0,
-        101
-    ])('should throw an Error when given %d', (param) => {
-        expect(() => fizzBuzzChecker(param)).toThrowError(errorMessage);
-    });
+  it.each([
+    102,
+    -12,
+    0,
+    101
+  ])('should throw an Error when given %d', (param) => {
+    expect(() => fizzBuzzChecker(param)).toThrowError(errorMessage);
+  });
 
-    it.each([
-        [3, 'Fizz'],
-        [9, 'Fizz'],
-        [42, 'Fizz'],
-        [5, 'Buzz'],
-        [10, 'Buzz'],
-        [20, 'Buzz'],
-        [15, 'FizzBuzz'],
-        [45, 'FizzBuzz'],
-        [60, 'FizzBuzz'],
-        [43, '43'],
-        [26, '26'],
-        [13, '13'],
-    ])('when given %d should return %s', (param, expected) => {
-        expect(fizzBuzzChecker(param)).toBe(expected);
-    });
+  it.each([
+    [3, 'Fizz'],
+    [9, 'Fizz'],
+    [42, 'Fizz'],
+    [5, 'Buzz'],
+    [10, 'Buzz'],
+    [20, 'Buzz'],
+    [15, 'FizzBuzz'],
+    [45, 'FizzBuzz'],
+    [60, 'FizzBuzz'],
+    [43, '43'],
+    [26, '26'],
+    [13, '13'],
+  ])('when given %d should return %s', (param, expected) => {
+    expect(fizzBuzzChecker(param)).toBe(expected);
+  });
 });
