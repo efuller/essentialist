26c8277146a54d1dcc828e9674a0a16bb0b00e01
Eric Fuller <info@glassmoonmedia.com>
2023-06-12 20:37:46 -0400

    [red]: should know that mom is a palindrome

--- 1_The_Physical/Getting_Started_Emergent_Design/1_Red_Green_Refactor/1_2_Palindrome/src/index.spec.ts
+++ 1_The_Physical/Getting_Started_Emergent_Design/1_Red_Green_Refactor/1_2_Palindrome/src/index.spec.ts
@@ -3,5 +3,9 @@ function palindromeChecker() {}
 describe('palindrome checker', () => {
   it('should know that palindromeChecker exists', () => {
     expect(palindromeChecker).toBeDefined();
-  })
+  });
+
+  it('should know that mom is a palindrome', () => {
+    expect(palindromeChecker('mom')).toBeTruthy();
+  });
 })
