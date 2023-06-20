09c95484b204139d8e4fc73c13aa120166929f81
Eric Fuller <info@glassmoonmedia.com>
2023-06-20 17:43:35 -0400

    [refactor]: group truthy tests

--- 1_The_Physical/Getting_Started_Emergent_Design/1_Red_Green_Refactor/1_2_Palindrome/src/index.spec.ts
+++ 1_The_Physical/Getting_Started_Emergent_Design/1_Red_Green_Refactor/1_2_Palindrome/src/index.spec.ts
@@ -5,8 +5,16 @@ describe('palindrome checker', () => {
     expect(palindromeChecker).toBeDefined();
   });
 
-  it('should know that mom is a palindrome', () => {
-    expect(palindromeChecker('mom')).toBeTruthy();
+  it.each([
+    'mom',
+    'Mom',
+    'Was It A Rat I Saw',
+    'MoM',
+    'xMomx',
+    'Never Odd or Even',
+    '1Never Odd or Even1'
+  ])('when given %s should return true', (param: string) => {
+    expect(palindromeChecker(param)).toBeTruthy();
   });
 
   it('should know that john is not a palindrome', () => {
@@ -21,35 +29,11 @@ describe('palindrome checker', () => {
     expect(palindromeChecker('lucy')).toBeFalsy();
   });
 
-  it('should know that Mom is a palindrome', () => {
-    expect(palindromeChecker('Mom')).toBeTruthy();
-  });
-
-  it('should know that Was It A Rat I Saw is a palindrome', () => {
-    expect(palindromeChecker('Was It A Rat I Saw')).toBeTruthy();
-  });
-
-  it('should know that MoM is a palindrome', () => {
-    expect(palindromeChecker('MoM')).toBeTruthy();
-  });
-
   it('should know that Momx is not a palindrome', () => {
     expect(palindromeChecker('Momx')).toBeFalsy();
-  });
-
-  it('should know that xMomx is a palindrome', () => {
-    expect(palindromeChecker('xMomx')).toBeTruthy();
-  });
-
-  it('should know that Never Odd or Even is a palindrome', () => {
-    expect(palindromeChecker('Never Odd or Even')).toBeTruthy();
   });
 
   it('should know that Never Odd or Even1 is not a palindrome', () => {
     expect(palindromeChecker('Never Odd or Even1')).toBeFalsy();
   });
-
-  it('should know that 1Never Odd or Even1 is a palindrome', () => {
-    expect(palindromeChecker('1Never Odd or Even1')).toBeTruthy();
-  });
-})
+});
