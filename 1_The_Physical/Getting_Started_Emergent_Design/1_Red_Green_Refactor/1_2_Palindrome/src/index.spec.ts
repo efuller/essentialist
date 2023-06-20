058d6f66b0c4080b7f189d0ebd397855c9dfb122
Eric Fuller <info@glassmoonmedia.com>
2023-06-20 17:46:57 -0400

    [refactor]: group falsy tests

--- 1_The_Physical/Getting_Started_Emergent_Design/1_Red_Green_Refactor/1_2_Palindrome/src/index.spec.ts
+++ 1_The_Physical/Getting_Started_Emergent_Design/1_Red_Green_Refactor/1_2_Palindrome/src/index.spec.ts
@@ -17,23 +17,13 @@ describe('palindrome checker', () => {
     expect(palindromeChecker(param)).toBeTruthy();
   });
 
-  it('should know that john is not a palindrome', () => {
-    expect(palindromeChecker('john')).toBeFalsy();
-  });
-
-  it('should know that jeff is not a palindrome', () => {
-    expect(palindromeChecker('jeff')).toBeFalsy();
-  });
-
-  it('should know that lucy is not a palindrome', () => {
-    expect(palindromeChecker('lucy')).toBeFalsy();
-  });
-
-  it('should know that Momx is not a palindrome', () => {
-    expect(palindromeChecker('Momx')).toBeFalsy();
-  });
-
-  it('should know that Never Odd or Even1 is not a palindrome', () => {
-    expect(palindromeChecker('Never Odd or Even1')).toBeFalsy();
+  it.each([
+    'john',
+    'jeff',
+    'lucy',
+    'Momx',
+    'Never Odd or Even1',
+  ])('when given %s should return false', (param: string) => {
+    expect(palindromeChecker(param)).toBeFalsy();
   });
 });
