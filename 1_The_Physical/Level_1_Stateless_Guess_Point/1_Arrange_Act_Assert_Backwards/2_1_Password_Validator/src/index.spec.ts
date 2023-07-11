8896819e370e59b8d9c231a2520aaac7f7110feb
Eric Fuller <info@glassmoonmedia.com>
2023-07-10 21:12:48 -0400

    [new]: it should know that `thePhysical1234567` exceeds the allowed number of characters

--- 1_The_Physical/Level_1_Stateless_Guess_Point/1_Arrange_Act_Assert_Backwards/2_1_Password_Validator/src/index.spec.ts
+++ 1_The_Physical/Level_1_Stateless_Guess_Point/1_Arrange_Act_Assert_Backwards/2_1_Password_Validator/src/index.spec.ts
@@ -8,8 +8,17 @@ describe('password validator', () => {
     expect(result).toMatchObject({
       isValid: false,
       errors: [ { type: 'NoMinimumCharacters', message: 'Password must contain at least 5 characters.' }]
-    })
-  })
+    });
+  });
+
+  it('should know that `thePhysical1234567` is not valid because it contains more than 15 characters', () => {
+    const result = passwordValidator('thePhysical1234567');
+
+    expect(result).toMatchObject({
+      isValid: false,
+      errors: [ { type: 'ExceedsCharacterLimit', message: 'Password cannot exceed 15 characters.' }]
+    });
+  });
 })
 
 
--- 1_The_Physical/Level_1_Stateless_Guess_Point/1_Arrange_Act_Assert_Backwards/2_1_Password_Validator/src/index.ts
+++ 1_The_Physical/Level_1_Stateless_Guess_Point/1_Arrange_Act_Assert_Backwards/2_1_Password_Validator/src/index.ts
@@ -1,6 +1,13 @@
 export function passwordValidator(password: string) {
+  if (password.length < 5 ) {
+    return {
+      isValid: false,
+      errors: [ { type: 'NoMinimumCharacters', message: 'Password must contain at least 5 characters.' }]
+    };
+  }
+
   return {
     isValid: false,
-    errors: [ { type: 'NoMinimumCharacters', message: 'Password must contain at least 5 characters.' }]
+    errors: [ { type: 'ExceedsCharacterLimit', message: 'Password cannot exceed 15 characters.' }]
   };
 }
