d8c9c6131f13f4eb5d546bc47dc290b03dbe08c3
Eric Fuller <info@glassmoonmedia.com>
2023-07-15 07:37:23 -0400

    [refactor]: refactor error types and set message to type string

--- 1_The_Physical/Level_1_Stateless_Guess_Point/1_Arrange_Act_Assert_Backwards/2_1_Password_Validator/src/index.ts
+++ 1_The_Physical/Level_1_Stateless_Guess_Point/1_Arrange_Act_Assert_Backwards/2_1_Password_Validator/src/index.ts
@@ -3,10 +3,12 @@ export type ResultObject = {
   errors: PasswordValidatorErrors[];
 }
 
-type PasswordValidatorErrors = { type: 'NoMinimumCharacters', message: 'Password must contain at least 5 characters.' }
-| { type: 'NoDigit', message: 'Password should contain at least one digit.' }
-| { type: 'NoUpperCase', message: 'Password should contain at least one upper case character.' }
-| { type: 'ExceedsCharacterLimit', message: 'Password cannot exceed 15 characters.' }
+type PasswordErrorTypes = 'NoMinimumCharacters'
+| 'NoDigit'
+| 'NoUpperCase'
+| 'ExceedsCharacterLimit'
+
+type PasswordValidatorErrors = { type: PasswordErrorTypes, message: string }
 
 export function passwordValidator(password: string): ResultObject {
   const response: ResultObject = {
