b2c16554250f5ba6b4b1ef9894207d357ef06a1c
Eric Fuller <info@glassmoonmedia.com>
2023-08-03 06:05:16 -0400

    [chores]: formatting

--- 1_The_Physical/Level_1_Stateless_Guess_Point/1_Arrange_Act_Assert_Backwards/2_2_Stats_Calculator/src/index.spec.ts
+++ 1_The_Physical/Level_1_Stateless_Guess_Point/1_Arrange_Act_Assert_Backwards/2_2_Stats_Calculator/src/index.spec.ts
@@ -2,14 +2,14 @@ import { statsCalculator, StatsResult } from "./index";
 
 describe('stats calculator', () => {
   it.each([
-    {given: [2,4,21,-8,53,40], stat: 'average', expected: 18.666666666667},
-    {given: [2,4,21,-8,53,40], stat: 'count', expected: 6},
-    {given: [2,4,21,-8,53,40], stat: 'max', expected: 53},
-    {given: [2,4,21,-8,53,40], stat: 'min', expected: -8},
-    {given: [4,-3,-7,25], stat: 'average', expected: 4.75},
-    {given: [4,-3,-7,25], stat: 'count', expected: 4},
-    {given: [4,-3,-7,25], stat: 'max', expected: 25},
-    {given: [4,-3,-7,25], stat: 'min', expected: -7},
+    { given: [2,4,21,-8,53,40], stat: 'average', expected: 18.666666666667 },
+    { given: [2,4,21,-8,53,40], stat: 'count', expected: 6 },
+    { given: [2,4,21,-8,53,40], stat: 'max', expected: 53 },
+    { given: [2,4,21,-8,53,40], stat: 'min', expected: -8 },
+    { given: [4,-3,-7,25], stat: 'average', expected: 4.75 },
+    { given: [4,-3,-7,25], stat: 'count', expected: 4 },
+    { given: [4,-3,-7,25], stat: 'max', expected: 25 },
+    { given: [4,-3,-7,25], stat: 'min', expected: -7 },
   ])('given an array of $given, the $stat value is $expected', (param) => {
     const results = statsCalculator(param.given);
     expect(results[param.stat as keyof StatsResult]).toBe(param.expected);
