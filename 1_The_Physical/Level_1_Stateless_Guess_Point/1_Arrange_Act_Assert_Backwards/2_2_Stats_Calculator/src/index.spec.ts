4347dbd40ceccfb7094c079cb97c1839e7ce1f21
Eric Fuller <info@glassmoonmedia.com>
2023-08-02 20:22:12 -0400

    [refactor]: update test description

--- 1_The_Physical/Level_1_Stateless_Guess_Point/1_Arrange_Act_Assert_Backwards/2_2_Stats_Calculator/src/index.spec.ts
+++ 1_The_Physical/Level_1_Stateless_Guess_Point/1_Arrange_Act_Assert_Backwards/2_2_Stats_Calculator/src/index.spec.ts
@@ -1,8 +1,8 @@
 import { statsCalculator } from "./index";
 
 describe('stats calculator', () => {
-  it('knows that given an array of 6 numbers the count should be 6', () => {
-    const input = [2,4,21,-8,53,40]
+  it('given an array of [2,4,21,-8,53,40], it can calculate the average, max, min, and element count', () => {
+    const input = [2,4,21,-8,53,40];
     const results = statsCalculator(input);
 
     expect(results.average).toBe(18.666666666667);
