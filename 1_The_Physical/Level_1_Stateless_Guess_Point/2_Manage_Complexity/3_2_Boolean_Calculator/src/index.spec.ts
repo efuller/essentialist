a1c2353c767baae6a47e76e084684bbbb8263e21
Eric Fuller <info@glassmoonmedia.com>
2023-08-28 19:58:05 -0400

    [new]: it should know that given 'TRUE' returns true

--- 1_The_Physical/Level_1_Stateless_Guess_Point/2_Manage_Complexity/3_2_Boolean_Calculator/src/index.spec.ts
+++ 1_The_Physical/Level_1_Stateless_Guess_Point/2_Manage_Complexity/3_2_Boolean_Calculator/src/index.spec.ts
@@ -1,4 +1,9 @@
+import { booleanCalculator } from "./index";
 
 describe('boolean calculator', () => {
+  it('should know that given "TRUE" should return true', () => {
+    const result = booleanCalculator("TRUE");
 
-})
+    expect(result).toBe(true);
+  });
+});
